28d9760ce025d1274766bfff37a1d7ee8dd8d36c
niloofar sadeghi <niloofar.sadeghi@re-work.dev>
2023-09-21 16:41:54 +0330

    feat: added absoloute paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
+const ALIASES = {
+  Contexts: resolve(__dirname, "./src/contexts"),
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  /* Absoloute path */
+  resolve: { alias: ALIASES },
+});
